40717b975504995d034ee59e301a90f3ea6a85cf
yishai <yishai5555@gmail.com>
2016-07-06 16:02:33 +0300

    b4

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,24 @@ const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 
+const paths = {
+    es6: ['src/**/*.js'],
+    es5: 'dist'
+};
+
+
+
 gulp.task('default', () => {
-    return gulp.src('src/**/*.js')
+    return gulp.src(paths.es6)
         .pipe(sourcemaps.init())
         .pipe(babel({
             presets: ['es2015']
         }))
         .pipe(concat('all.js'))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest(paths.es5));
+});
+
+gulp.task('watch', function() { // (D)
+    gulp.watch(paths.es6, ['babel']);
 });
