58f652f03c5cee7da666369ade72b3e8f8fe9979
yishai <yishai5555@gmail.com>
2016-07-06 16:11:57 +0300

    b5

--- dist/all.js
+++ dist/all.js
@@ -3,11 +3,24 @@
 var calculateMonthlyPayment = function calculateMonthlyPayment(principal, years, rate) {
     var monthlyRate = 0;
 
+    var name = 'yishai';
+
+    debugger;
+
     if (rate) {
         monthlyRate = rate / 100 / 12;
     }
     var monthlyPayment = principal * monthlyRate / (1 - Math.pow(1 / (1 + monthlyRate), years * 12));
-    return monthlyPayment;
+
+    // return monthlyPayment;
+
+    return {
+        principal: principal,
+        years: years,
+        rate: rate,
+        monthlyPayment: monthlyPayment,
+        monthlyRate: monthlyRate
+    };
 };
 
 document.getElementById('calcBtn').addEventListener('click', function () {
@@ -15,6 +28,9 @@ document.getElementById('calcBtn').addEventListener('click', function () {
     var years = document.getElementById("years").value;
     var rate = document.getElementById("rate").value;
     var monthlyPayment = calculateMonthlyPayment(principal, years, rate);
-    document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
+
+    // document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
+
+    document.getElementById("monthlyPayment").innerHTML = monthlyPayment.monthlyPayment.toFixed(2);
 });
 //# sourceMappingURL=all.js.map
--- src/main.js
+++ src/main.js
@@ -1,11 +1,26 @@
-let calculateMonthlyPayment = function (principal, years, rate) {
+let calculateMonthlyPayment = function(principal, years, rate) {
     let monthlyRate = 0;
+
+    let name='yishai';
+
+    debugger;
 
     if (rate) {
         monthlyRate = rate / 100 / 12;
     }
-    let monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
-    return monthlyPayment;
+    let monthlyPayment = principal * monthlyRate /
+        (1 - (Math.pow(1/(1 + monthlyRate), years * 12)));
+
+    // return monthlyPayment;
+
+    return {
+        principal: principal,
+        years: years,
+        rate: rate,
+        monthlyPayment: monthlyPayment,
+        monthlyRate: monthlyRate
+    };
+
 };
 
 document.getElementById('calcBtn').addEventListener('click', function () {
@@ -13,7 +28,9 @@ document.getElementById('calcBtn').addEventListener('click', function () {
     let years = document.getElementById("years").value;
     let rate = document.getElementById("rate").value;
     let monthlyPayment = calculateMonthlyPayment(principal, years, rate);
-    document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
+
+    // document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
+
+    document.getElementById("monthlyPayment").innerHTML = monthlyPayment.monthlyPayment.toFixed(2);
 });
 
-//let x-"test";
