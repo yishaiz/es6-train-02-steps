2f73eb483335d36b0b6f537540ce276cbea3124a
yishai <yishai5555@gmail.com>
2016-07-06 16:25:30 +0300

    b7

--- dist/all.js
+++ dist/all.js
@@ -1,21 +1,15 @@
 'use strict';
 
+//let calculateMonthlyPayment = function(principal, years, rate) {
 var calculateMonthlyPayment = function calculateMonthlyPayment(principal, years, rate) {
     var monthlyRate = 0;
-
-    var name = 'yishai';
-
-    debugger;
 
     if (rate) {
         monthlyRate = rate / 100 / 12;
     }
     var monthlyPayment = principal * monthlyRate / (1 - Math.pow(1 / (1 + monthlyRate), years * 12));
 
-    // return monthlyPayment;
-
-    return {
-        principal: principal,
+    return { principal: principal,
         years: years,
         rate: rate,
         monthlyPayment: monthlyPayment,
@@ -23,20 +17,45 @@ var calculateMonthlyPayment = function calculateMonthlyPayment(principal, years,
     };
 };
 
+var calculateAmortization = function calculateAmortization(principal, years, rate) {
+    var _calculateMonthlyPaym = calculateMonthlyPayment(principal, years, rate);
+
+    var monthlyRate = _calculateMonthlyPaym.monthlyRate;
+    var monthlyPayment = _calculateMonthlyPaym.monthlyPayment;
+
+    var balance = principal;
+    var amortization = [];
+    for (var y = 0; y < years; y++) {
+        var interestY = 0; //Interest payment for year y
+        var principalY = 0; //Principal payment for year y
+        for (var m = 0; m < 12; m++) {
+            var interestM = balance * monthlyRate; //Interest payment for month m
+            var principalM = monthlyPayment - interestM; //Principal payment for month m
+            interestY = interestY + interestM;
+            principalY = principalY + principalM;
+            balance = balance - principalM;
+        }
+        amortization.push({ principalY: principalY, interestY: interestY, balance: balance });
+    }
+    return { monthlyPayment: monthlyPayment, monthlyRate: monthlyRate, amortization: amortization };
+};
+
 document.getElementById('calcBtn').addEventListener('click', function () {
     var principal = document.getElementById("principal").value;
     var years = document.getElementById("years").value;
     var rate = document.getElementById("rate").value;
 
-    //    let monthlyPayment = calculateMonthlyPayment(principal, years, rate);
+    var _calculateAmortizatio = calculateAmortization(principal, years, rate);
 
-    var _calculateMonthlyPaym = calculateMonthlyPayment(principal, years, rate);
+    var monthlyPayment = _calculateAmortizatio.monthlyPayment;
+    var monthlyRate = _calculateAmortizatio.monthlyRate;
+    var amortization = _calculateAmortizatio.amortization;
 
-    var monthlyPayment = _calculateMonthlyPaym.monthlyPayment;
-    var monthlyRate = _calculateMonthlyPaym.monthlyRate;
-
-    // document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
 
     document.getElementById("monthlyPayment").innerHTML = monthlyPayment;
+
+    amortization.forEach(function (month) {
+        return console.log(month);
+    });
 });
 //# sourceMappingURL=all.js.map
--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
-let calculateMonthlyPayment = function(principal, years, rate) {
+//let calculateMonthlyPayment = function(principal, years, rate) {
+let calculateMonthlyPayment = (principal, years, rate) => {
     let monthlyRate = 0;
-
-    let name='yishai';
-
-    debugger;
 
     if (rate) {
         monthlyRate = rate / 100 / 12;
@@ -11,31 +8,48 @@ let calculateMonthlyPayment = function(principal, years, rate) {
     let monthlyPayment = principal * monthlyRate /
         (1 - (Math.pow(1/(1 + monthlyRate), years * 12)));
 
-    // return monthlyPayment;
-
-    return {
-        principal: principal,
-        years: years,
-        rate: rate,
-        monthlyPayment: monthlyPayment,
-        monthlyRate: monthlyRate
-    };
+    return {principal,
+        years,
+        rate,
+        monthlyPayment,
+        monthlyRate
+    }
 
 };
 
-document.getElementById('calcBtn').addEventListener('click', function () {
+let calculateAmortization = (principal, years, rate) => {
+    let {monthlyRate, monthlyPayment} = calculateMonthlyPayment(principal, years, rate);
+    let balance = principal;
+    let amortization = [];
+    for (let y=0; y<years; y++) {
+        let interestY = 0;  //Interest payment for year y
+        let principalY = 0; //Principal payment for year y
+        for (let m=0; m<12; m++) {
+            let interestM = balance * monthlyRate;       //Interest payment for month m
+            let principalM = monthlyPayment - interestM; //Principal payment for month m
+            interestY = interestY + interestM;
+            principalY = principalY + principalM;
+            balance = balance - principalM;
+        }
+        amortization.push({principalY, interestY, balance});
+    }
+    return {monthlyPayment, monthlyRate, amortization};
+};
+
+
+document.getElementById('calcBtn').addEventListener('click', ()=> {
     let principal = document.getElementById("principal").value;
     let years = document.getElementById("years").value;
     let rate = document.getElementById("rate").value;
 
 
-//    let monthlyPayment = calculateMonthlyPayment(principal, years, rate);
-    let {monthlyPayment, monthlyRate} =
-        calculateMonthlyPayment(principal, years, rate);
+ let {monthlyPayment, monthlyRate, amortization} = calculateAmortization (principal, years, rate);
 
-
-    // document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
 
     document.getElementById("monthlyPayment").innerHTML = monthlyPayment;
+
+
+amortization.forEach(month =>
+    console.log(month));
 });
 
